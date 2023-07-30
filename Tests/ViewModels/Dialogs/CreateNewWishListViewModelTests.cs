634162999aef65d951c76f2961f6788b408b9605
Ross O'Donegan <rossodonegan123@gmail.com>
2023-07-30 11:46:53 +0100

    Added unit tests for create wishlist validation

--- Tests/ViewModels/Dialogs/CreateNewWishListViewModelTests.cs
+++ Tests/ViewModels/Dialogs/CreateNewWishListViewModelTests.cs
@@ -44,7 +44,16 @@ namespace Tests.ViewModels.Dialogs
         [TestMethod()]
         public void AttemptToCreateWishListCommandShouldNavigateToViewWishListOnSuccessfulValidation()
         {
-            //Add mock validation data once added
+            //Arrange
+            WishListViewModel model = new()
+            {
+                Title = "testTitle",
+                Country = "testCountry",
+                StartDate = "21/03/23",
+                EndDate = "28/03/23"
+            };
+
+            _createNewWishListViewModel.WishListViewModel = model;
 
             // Act
             _createNewWishListViewModel.AttemptToCreateWishListCommand.Execute(null);
@@ -83,6 +92,36 @@ namespace Tests.ViewModels.Dialogs
             Assert.IsTrue(expectedWishList.Country == "testCountry");
             Assert.IsTrue(expectedWishList.StartDate == "21/03/2023"); //Parsing DateOnly from model -> produces full year
             Assert.IsTrue(expectedWishList.EndDate == "28/03/2023");
+        }
+
+        [DataRow("Ttle", "Sweden", "12/05/23", "19/05/23", "Stockholm", "Stockholm Visit")] //Title 
+        [DataRow("Title", "", "12/05/23", "19/05/23", "Stockholm", "Stockholm Visit")] //Country 
+        [DataRow("Title", "Sweden", "21/05/23", "19/05/23", "Stockholm", "Stockholm Visit")] //StartDate 
+        [DataRow("Title", "Sweden", "12/05/23", "10/05/23", "Stockholm", "Stockholm Visit")] //EndDate 
+        [DataRow("Title", "Sweden", "12/05/23", "19/05/23", "Stockholm", "Sto")] //Desc 
+        [DataTestMethod()]
+        public void CreateWishListlValidationShouldFailWithIllegalEntries(string title, string country, string startDate, string endDate,
+            string city, string desc)
+        {
+            //Arrange
+            var model = new WishListViewModel()
+            {
+                Title = title,
+                Country = country,
+                StartDate = startDate,
+                EndDate = endDate,
+                City = city,
+                Description = desc
+            };
+
+            _createNewWishListViewModel.WishListViewModel = model;
+
+            //Act
+            _createNewWishListViewModel.AttemptToCreateWishListCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(_createNewWishListViewModel.WishListViewModel.HasErrors);
+            Assert.IsNotInstanceOfType(_mainWindowViewModel.CurrentViewModel, typeof(ViewWishListViewModel));
         }
     }
 }
