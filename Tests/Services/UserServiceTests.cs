d21956236e7edf9fad2bc331acb4c33251d50867
Ross O'Donegan <rossodonegan123@gmail.com>
2023-08-03 20:26:30 +0100

    UserService.Register tests

--- Tests/Services/UserServiceTests.cs
+++ Tests/Services/UserServiceTests.cs
@@ -11,6 +11,7 @@ using Services.Mappers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using ViewModels;
@@ -45,7 +46,7 @@ namespace Tests.Services
             var userName = "TestUsername";
             var password = "password123";
 
-            var userId = Guid.Parse("67ce7098-0dc3-4353-b493-765ad533fe41");    
+            var userId = Guid.Parse("67ce7098-0dc3-4353-b493-765ad533fe41");
             _userRepository.Setup(x => x.Login(userName, password))
                 .Returns(new ResultDto() { Success = true, Id = userId });
 
@@ -83,7 +84,56 @@ namespace Tests.Services
 
             //Assert
             _userRepository.Verify(x => x.Login(userName, password));
-            Assert.IsTrue(result.IsExistingUser == false);
+            Assert.IsFalse(result.IsExistingUser);
+        }
+
+        [TestMethod()]
+        public void RegisterWithNonExistingCredentialsReturnsHydratedResultDto()
+        {
+            //Arrange
+            var userDto = new UserDto()
+            {
+                Username = "TestUsername",
+                Password = "password123",
+                FirstName = "Test",
+                EmailAddress = "test@gmail.com"
+            };
+
+            var userId = Guid.Parse("67ce7098-0dc3-4353-b493-765ad533fe41");
+            _userRepository.Setup(x => x.Register(userDto)).Returns(new ResultDto() { Id = userId, Success = true });
+
+            //Act
+            var result = _userService.Register(userDto);
+
+            //Assert
+            _userRepository.Verify(x => x.DoesUserAlreadyExist("TestUsername"));
+            _userRepository.Verify(x => x.Register(userDto));
+            Assert.IsTrue(result.Id == userId);
+            Assert.IsTrue(result.Success == true);
+        }
+
+        [TestMethod()]
+        public void RegisterWithExistingCredentialsReturnsFalseSuccessResultDto()
+        {
+            //Arrange
+            var userDto = new UserDto()
+            {
+                Username = "TestUsername",
+                Password = "password123",
+                FirstName = "Test",
+                EmailAddress = "test@gmail.com"
+            };
+
+            _userRepository.Setup(x => x.Register(userDto)).Returns(new ResultDto() { Success = false });
+            _userRepository.Setup(x => x.DoesUserAlreadyExist("TestUsername")).Returns(true);
+
+            //Act
+            var result = _userService.Register(userDto);
+
+            //Assert
+            _userRepository.Verify(x => x.DoesUserAlreadyExist("TestUsername"));
+            _userRepository.VerifyNoOtherCalls();
+            Assert.IsFalse(result.Success);
         }
     }
 
